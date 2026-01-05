027d9bcd65810e838485dd298bc4407b6ba4a19e
Yan <62499077+XianYin69@users.noreply.github.com>
2026-01-05 22:02:33 +0800

    Add something

--- OfficePlus/Pages/OfficePlusPage.cs
+++ OfficePlus/Pages/OfficePlusPage.cs
@@ -2,9 +2,14 @@
 // The Microsoft Corporation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
 using OfficePlus.Pages;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace OfficePlus;
 
@@ -17,10 +22,40 @@ internal sealed partial class OfficePlusPage : ListPage
         Name = "Open or creat your file";
     }
 
+    //getOfficeRecentFiles
+    private List<Results> getOfficeRecentFiles(string search)
+    {
+        var results = new List<Result>();
+        //Local File
+        string officeRecentPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            @"Microsoft\Office\Recent"
+        );
+        if (Directory.Exists(officeRecentPath))
+        {
+            DirectoryInfo dir = new DirectoryInfo(officeRecentPath);
+            var file = dir
+                .GetFiles("*.lnk")
+                .OrderByDescending(f => f.LastWriteTime)
+                .Take(20);
+        }
+
+        foreach (var file in files)
+        {
+            //filter links
+            if (string.IsNullOrEmpty(search) || file.Name.ToLower().Contains(search.ToLower()))
+            {
+                results.Add(file);
+            }
+        }
+        {
+            
+        }
+    }
     public override IListItem[] GetItems()
     {
         return [
-            new ListItem(new NoOpCommand()) { Title = "注意：这是一个非微软官方拓展" },
+            new ListItem(new NoOpCommand()) { Title = "!!!娉ㄦ剰锛氭湰鎵╁睍涓洪潪寰蒋瀹樻柟鎵╁睍!!!" },
             new ListItem(new WordPage()) { Title = "Word" },
             new ListItem(new ExcelPage ()) { Title = "Excel" },
             new ListItem(new PowerPointPage()) { Title = "PowerPoint" }
