9cb433e514af8592b1e03048e1b94d7e02ce3c53
XianYin69 <www324608yf@outlook.com>
2026-01-12 09:36:54 +0800

    add more language support

--- OfficePlus/Pages/OfficePlusPage.cs
+++ OfficePlus/Pages/OfficePlusPage.cs
@@ -9,6 +9,8 @@ using System.IO;
 using System.Linq;
 using Microsoft.CommandPalette.Extensions;
 using Microsoft.CommandPalette.Extensions.Toolkit;
+using System.Text.Json;
+using System.Globalization;
 
 namespace OfficePlus.Pages;
 
@@ -66,5 +68,107 @@ internal sealed partial class OfficePlusPage : ListPage
         }
 
         return items.ToArray();
+    }
+}
+
+//i18n support
+public static class i18n
+{
+    private static JsonElement _currentLanguageData;
+    private static readonly string DefaultLang = "en-US";
+    static i18n()
+    {
+        try
+        {
+            string jsonPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Language.json");
+            string jsonContent = File.ReadAllText(jsonPath);
+            using var doc  = JsonDocument.Parse(jsonContent);
+            var root = doc.RootElement;
+            // get system language
+            string uiCulture = CultureInfo.CurrentUICulture.Name;
+            //Matching Logic Priority
+            //Exact Match
+            if (root.TryGetProperty(uiCulture, out var exacMatch))
+            {
+                _currentLanguageData = exacMatch.Clone();
+            }
+            //Language Group Match
+            else if (uiCulture.StartsWith("zh") && root.TryGetProperty("zh-TW", out var zhFallBack))
+            {
+                _currentLanguageData = zhFallBack.Clone();
+            }
+            //Base Languagge Match
+            else if (root.TryGetProperty(uiCulture.Split("-")[0], out var baseLanguage))
+            {
+                _currentLanguageData = baseLanguage.Clone();
+            }
+            //Back to English
+            else
+            {
+                _currentLanguageData = root.GetProperty(DefaultLang).Clone();
+            }
+        }
+        catch
+        {
+            //code fix languge
+            string fallbackjson = @"{
+               ""en-US"": {
+                    ""OfficePlusPage"": {
+                    ""Item2"": ""Word"",
+                    ""Item3"": ""Excel"",
+                    ""Item4"": ""PowerPoint"",
+                    ""Item1"": ""WARNING: it is an unofficial plugin for Microsoft Office""
+                    },
+                    ""WordPage"": {
+                        ""CreateNewDoc"": ""Create New Document"",
+                        ""CreateNewDocSub"": ""Open Microsoft Word and create a new document"",
+                        ""OpenDoc"": ""Open Document"",
+                        ""OpenDocSub"": ""Open recent document"",
+                        ""WordRecentPage"": ""Recent Documents""
+                    },
+                    ""ExcelPage"": {
+                        ""CreateNewDoc"": ""Create New Workbook"",
+                        ""CreateNewDocSub"": ""Open Microsoft Excel and create a new workbook"",
+                        ""OpenDoc"": ""Open Workbook"",
+                        ""OpenDocSub"": ""Open recent workbook"",
+                        ""WordRecentPage"": ""Recent Workbooks""
+                    },
+                    ""PowerPointPage"": {
+                        ""CreateNewDoc"": ""Create New Presentation"",
+                        ""CreateNewDocSub"": ""Open Microsoft PowerPoint and create a new presentation"",
+                        ""OpenDoc"": ""Open Presentation"",
+                        ""OpenDocSub"": ""Open recent presentation"",
+                        ""WordRecentPage"": ""Recent Presentations""
+                    }
+                }     
+            }";
+            var fallbackDoc = JsonDocument.Parse(fallbackjson);
+            _currentLanguageData = fallbackDoc.RootElement.GetProperty(DefaultLang).Clone();
+        }
+    }
+    public static string T(string path)
+    {
+        try
+        {
+            if (string.IsNullOrEmpty(path)) return path;
+            string[] keys = path.Split('.');
+            JsonElement current = _currentLanguageData;
+            foreach (string key in keys)
+            {
+                if (current.ValueKind == JsonValueKind.Object && current.TryGetProperty(key, out JsonElement next))
+                {
+                    current = next;
+                }
+                else
+                {
+                    return path;
+                }
+            }
+            return current.ValueKind == JsonValueKind.String ? current.GetString() : path;
+        }
+        catch
+        {
+            return path;
+        }
     }
 }
