efc673890c818ebfb458a41870c7fc7d040b06a1
XianYin69 <www324608yf@outlook.com>
2026-03-16 18:01:25 +0800

    Update "Settings.cs"

---
+++ SuperSpace/Pages/SuperSpaceSettingsPage/SuperSpaceSettingsPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+
+namespace SuperSpace.Pages.SuperSpaceSettingPage
+{
+    internal sealed partial class SuperSpaceSettingPage : ContentPage
+    {
+        private readonly Settings _settings = new();
+        private readonly List<ChoiceSetSetting.Choice> _choices = new()
+        {
+            new ChoiceSetSetting.Choice("SteamCommonPath",""),
+            new ChoiceSetSetting.Choice("EpicGamePath","")
+        };
+        
+        public override IContent[] GetContent()
+        {
+            var s = _settings.ToContent();
+            return s;
+        }
+
+        public SuperSpaceSettingPage()
+        {
+            Name = "SuperSpaceSettings";
+            Icon = new IconInfo(string.Empty);
+            _settings.Add(new TextSetting("SteamGamePath", "")
+            {
+                Label = "Steam Common Folder Path:",
+                Description = ""
+            });
+            _settings.Add(new TextSetting("EpicGamePath", "")
+            {
+                Label = "Epic Game Folder Path:",
+                Description = ""
+            });
+        }
+    }
+}
