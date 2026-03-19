2306b9153095e4d69aa32685c232591341f6cc6e
Yan <62499077+XianYin69@users.noreply.github.com>
2026-03-19 15:25:56 +0800

    Add OpenApp.cs and fix i18n.cs

---
+++ SuperSpace/Addition/OpenApp/OpenApp.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.CommandPalette.Extensions.Toolkit;
+using System.Diagnostics;
+
+namespace SuperSpace.Addition.OpenApp;
+
+//Open application
+internal sealed partial class OpenApp : InvokableCommand
+{
+    private readonly string _excutable;
+
+    public OpenApp(string excutable)
+    {
+        _excutable = excutable;
+    }
+
+    public override CommandResult Invoke()
+    {
+        try
+        {
+            Process.Start(new ProcessStartInfo
+            {
+                FileName = _excutable,
+                UseShellExecute =true
+            });
+            return CommandResult.Dismiss();
+        } catch
+        {
+            return CommandResult.KeepOpen();
+        }
+    }
+}
