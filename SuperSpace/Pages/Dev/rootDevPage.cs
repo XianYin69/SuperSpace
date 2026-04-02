00e8194553a22b68bb6c0d25656c9c11e3818a9d
Yan <www324608yf@outlook.com>
2026-04-02 15:08:34 +0800

    Review "SuperSpace.csproj"

---
+++ SuperSpace/Pages/Dev/rootDevPage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.CommandPalette.Extensions;
+using Microsoft.CommandPalette.Extensions.Toolkit;
+using static SuperSpace.Addition.i18n.i18n;
+
+
+namespace SuperSpace.Pages.Dev;
+
+internal sealed partial class rootDevPage : ListPage
+{
+    public rootDevPage()
+    {
+        Icon = IconHelpers.FromRelativePath("Assets\\");
+        Title = T("");
+    }
+
+    public override IListItem[] GetItems()
+    {
+        var items = new List<IListItem> { 
+            new ListItem(new NoOpCommand())
+            {
+                Icon = IconHelpers.FromRelativePath("Assets\\"),
+                Title = T(""),
+                Subtitle = T("")
+            }
+        };
+        return items.ToArray();
+    }
+}
